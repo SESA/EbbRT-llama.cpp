944147a94893e928f01fad675d9bd6153f58d9a4
hand32 <hand32@node0.hand32-206700.bayopsys-pg0.wisc.cloudlab.us>
2024-05-24 13:54:48 -0500

    built build-info.o

---
+++ src/common/build-info.cpp
@@ -0,0 +1,4 @@
+int LLAMA_BUILD_NUMBER = 2862;
+char const *LLAMA_COMMIT = "dc685be4";
+char const *LLAMA_COMPILER = "cc (Ubuntu 11.4.0-1ubuntu1~22.04) 11.4.0";
+char const *LLAMA_BUILD_TARGET = "x86_64-linux-gnu";
