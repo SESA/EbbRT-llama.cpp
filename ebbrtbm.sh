f632828992ecca246c7d7b6fae34fe08dfb8e4be
hand32 <hand32@node0.hand32-206700.bayopsys-pg0.wisc.cloudlab.us>
2024-05-24 13:46:46 -0500

    built grammar-parser.o

--- ebbrtbm.sh
+++ ebbrtbm.sh
@@ -12,4 +12,6 @@ SYSROOT=~/sysroot_llm/native
 
 #$CXX --sysroot=$SYSROOT -fPIC -O3 -Wall -Wextra -Wpedantic -Wcast-qual -Wno-unused-function -Wmissing-declarations -Wmissing-noreturn -march=native -mtune=native -Wno-array-bounds -Wno-format-truncation -Wextra-semi -Isrc -Isrc/common -D_XOPEN_SOURCE=600 -D_GNU_SOURCE -DNDEBUG -DGGML_USE_LLAMAFILE -D_EBBRT_ -c src/common/common.cpp -o src/common.o
 
-$CXX --sysroot=$SYSROOT -fPIC -O3 -Wall -Wextra -Wpedantic -Wcast-qual -Wno-unused-function -Wmissing-declarations -Wmissing-noreturn -march=native -mtune=native -Wno-array-bounds -Wno-format-truncation -Wextra-semi -Isrc -Isrc/common -D_XOPEN_SOURCE=600 -D_GNU_SOURCE -DNDEBUG -DGGML_USE_LLAMAFILE -D_EBBRT_ -c src/common/sampling.cpp -o src/sampling.o
+#$CXX --sysroot=$SYSROOT -fPIC -O3 -Wall -Wextra -Wpedantic -Wcast-qual -Wno-unused-function -Wmissing-declarations -Wmissing-noreturn -march=native -mtune=native -Wno-array-bounds -Wno-format-truncation -Wextra-semi -Isrc -Isrc/common -D_XOPEN_SOURCE=600 -D_GNU_SOURCE -DNDEBUG -DGGML_USE_LLAMAFILE -D_EBBRT_ -c src/common/sampling.cpp -o src/sampling.o
+
+$CXX --sysroot=$SYSROOT -fPIC -O3 -Wall -Wextra -Wpedantic -Wcast-qual -Wno-unused-function -Wmissing-declarations -Wmissing-noreturn -march=native -mtune=native -Wno-array-bounds -Wno-format-truncation -Wextra-semi -Isrc -Isrc/common -D_XOPEN_SOURCE=600 -D_GNU_SOURCE -DNDEBUG -DGGML_USE_LLAMAFILE -D_EBBRT_ -c src/common/grammar-parser.cpp -o src/grammar-parser.o
