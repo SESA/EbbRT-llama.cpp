6383243322a940b50ae15ebbc39272a923278164
hand32 <hand32@node0.hand32-206700.bayopsys-pg0.wisc.cloudlab.us>
2024-05-24 18:35:01 -0500

    built ggml-alloc.o

--- ebbrtbm.sh
+++ ebbrtbm.sh
@@ -20,4 +20,6 @@ SYSROOT=~/sysroot_llm/native
 
 #$CXX --sysroot=$SYSROOT -fPIC -O4 -flto -Wall -Wextra -Wpedantic -Wcast-qual -Wno-unused-function -Wmissing-declarations -Wmissing-noreturn -march=native -mtune=native -Wno-array-bounds -Wno-format-truncation -Wextra-semi -Isrc -Isrc/common -D_XOPEN_SOURCE=600 -D_GNU_SOURCE -DNDEBUG -DGGML_USE_LLAMAFILE -D_EBBRT_ -c src/common/json-schema-to-grammar.cpp -o src/json-schema-to-grammar.o
 
-$CXX --sysroot=$SYSROOT -fPIC -O4 -flto -Wall -Wextra -Wpedantic -Wcast-qual -Wno-unused-function -Wmissing-declarations -Wmissing-noreturn -march=native -mtune=native -Wno-array-bounds -Wno-format-truncation -Wextra-semi -Isrc -Isrc/common -D_XOPEN_SOURCE=600 -D_GNU_SOURCE -DNDEBUG -DGGML_USE_LLAMAFILE -D_EBBRT_ -c src/sgemm.cpp -o src/sgemm.o
+#$CXX --sysroot=$SYSROOT -fPIC -O4 -flto -Wall -Wextra -Wpedantic -Wcast-qual -Wno-unused-function -Wmissing-declarations -Wmissing-noreturn -march=native -mtune=native -Wno-array-bounds -Wno-format-truncation -Wextra-semi -Isrc -Isrc/common -D_XOPEN_SOURCE=600 -D_GNU_SOURCE -DNDEBUG -DGGML_USE_LLAMAFILE -D_EBBRT_ -c src/sgemm.cpp -o src/sgemm.o
+
+$CC --sysroot=$SYSROOT -Isrc -Isrc/common -D_XOPEN_SOURCE=600 -D_GNU_SOURCE -DNDEBUG -DGGML_USE_LLAMAFILE -D_EBBRT_ -std=c11   -fPIC -O4 -flto -Wall -Wextra -Wpedantic -Wcast-qual -Wno-unused-function -Wshadow -Wstrict-prototypes -Wpointer-arith -Wmissing-prototypes -Werror=implicit-int -Werror=implicit-function-declaration -march=native -mtune=native -Wdouble-promotion    -c src/ggml-alloc.c -o src/ggml-alloc.o
